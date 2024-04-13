ee277db6f47db98bfb5e9113f1c4b7a30fc462ee
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2024-04-13 18:24:17 +0300

    Add project files.

---
+++ TestxUnitProject.Tests/UserValidationTests.cs
@@ -0,0 +1,48 @@
+﻿namespace TestxUnitProject.Tests
+{
+    public class UserValidationTests
+    {
+        //TDD - Test Driven Development
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void UserValidation_IsNameValid_InvalidData(string name)
+        {
+            Assert.Throws<ArgumentException>(() => UserValidation.IsNameValid(name));
+        }
+
+
+        [Theory]
+        [InlineData("John123")]
+        [InlineData("123John")]
+        public void UserValidation_IsNameValid_InvalidName(string name)
+        {
+            // Triple A (Arrange-Act-Assert)
+            // Arrange
+            //string name = string.Empty;
+
+            // Act
+            bool result = UserValidation.IsNameValid(name);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void UserValidation_IsNameValid_ValidName()
+        {
+            // Triple A (Arrange-Act-Assert)
+            // Arrange
+            string name = "John";
+
+            // Act
+            bool result = UserValidation.IsNameValid(name);
+
+            // Assert
+            Assert.True(result);
+            //Assert.Throws<NotImplementedException>();
+        }
+    }
+}
---
+++ TestxUnitProject/UserValidation.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TestxUnitProject;
+
+public static class UserValidation
+{
+    public static bool IsNameValid(string name) 
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException(nameof(name));
+        }
+
+        return !name.Any(char.IsDigit);
+    }
+
+    public static bool IsEmailValid(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool IsPasswordValid(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    
+}
